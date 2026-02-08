daee8be65046f6fe5f489be0b05c0d0c32b2e41e
And-or1310 <andyhernandez994@gmail.com>
2026-02-08 01:44:28 -0800

    Commit prueba 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,12 @@
 
 import './App.css'
-import DYA from './DYA2.png'
-import dulcecita from './dulcecita.png'
+
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <img src={DYA} alt="DYA2" className="hero-image" />
-      <h1>Dulce + Andy</h1>
-
-      <div className="card">
-        
-        <p>
-
-          Puede que no tenga una razón que puedas entender ahorita corazón
-          o que yo te pueda explicar y poniéndome a pensar uno momento de
-          a deberás como dices, es como cuando somos niños y lloramos por
-          cosas que no entendemos, pero lloramos por miedo o porque nos
-          quitaron algo o porque algo no salió como queríamos, al final cuando
-          sentimos algo de chiquitos sigue siendo que estamos chiquitos.
-
-        </p>
-
-        <p>
-          Pero es mas feo y mas fuerte cuando uno crece porque uno no va
-          llorando o gritando nada mas a si por la vida porque ya entiende
-          mas cosas uno sabe que llorar por algo que no va a cambiar nomas
-          es llorar porque quieres llorar y tienes ganas de hacerlo y eso para mi
-          es mas fuerte todavía es mas todavía mas alarmante cuando entiendes algo
-          y aun a si uno se da el lujo de hacer esas cosas que no van a cambiar ese algo.
-
-        </p>
-
-        <p>
-          Se que te amo porque para mi es fácil estar lejos de ti cuando hay un tiempo que una vez se cumpla se que voy a verte y tengo que ser funcional y mantenerme como me dejaste hasta entonces, porque se que cuando te vea tu saltaras de lo mucho que me extrañaste y yo estaré como me dejaste para recibir tanto cariño tuyo. Pero se que te amo cuando es todo lo contrario a eso, cuando se me juntan los planes y las horas,  los días cambian y los asuntos se mueven y entonces hay algo, tras algo tras algo, que cambian mi hora de verte y es ahí cuando me permito sentir miedo, estar triste, molesto, porque se que aunque no podre cambiar esas cosas, quiero sentir todo eso madamas por querer nada mas porque estar lejos de ti sin saber cuando
-          van a cambiar las cosas me provoca eso.
-          Se que te amo porque cuando se trata de ti no quiero ser el que sabe que las cosas son a si nada mas y que no existe algo diferente, se que te amo porque quiero que para ti exista lo bonito lo diferente lo fácil lo mágico, porque no me imagino a ti mi pequeña solita por tanto tiempo sin que te cuiden, sin que revisen si tus papos están bien amarrados, si traes tu ropita bien puesta, si no se te olvida nada, si comiste si te llenaste si tienes azúcar encima, si te ensuciaste, como traes el cabellito, como te vestiste hoy cuanta popis hiciste.
-
-        </p>
-
-        <p>
-
-          Alomejor no son cosas tan complicadas o detalles que prepares pero amo como haces todas las cosas que puedo verte hacer todos los días, para mi cuando me preguntas si te amo, es muy ilógico porque nomas quiero decirte ¿Ya te viste abrochándote las agujetas? ¿si vez el tamaño de tu puñito? tu carita de enojada.? tu increíble musculatura.? nomas asi te amo por ser tu y como se que te amo pq tu sigues siendo tu peque
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Te amo Pequeñisisma
-      </p>
-      <img src={dulcecita} alt="dulcecita" className="hero-image" />
     </>
   )
 }
